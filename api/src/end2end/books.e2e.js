00a52ea66b3f983d59a957edd0de8ce95703b6b4
Facundo Carroz <facucarroz@gmail.com>
2022-12-26 21:48:31 -0300

    added

---
+++ api/src/end2end/books.e2e.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+const createApp = require('../app');
+const { generateManyBook } = require('../fakes/book.fake');
+
+const mockFakeBooks = generateManyBook(5);
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: () => mockFakeBooks,
+  create: () => {},
+})));
+
+describe('Test for books', () => {
+  let app = null;
+  let server = null;
+
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe('Test endpoint [GET] /api/v1/books', () => {
+    test('shounld return a list books', async () => request(app)
+      .get('/api/v1/books')
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.length).toEqual(5);
+      }));
+  });
+});
