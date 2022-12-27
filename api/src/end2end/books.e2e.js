0d1a5e27fb8707c0a1eb84bf7afc185e78969343
Facundo Carroz <facucarroz@gmail.com>
2022-12-26 23:36:03 -0300

    added

--- api/src/end2end/books.e2e.js
+++ api/src/end2end/books.e2e.js
@@ -1,32 +1,57 @@
 const request = require('supertest');
+const { MongoClient } = require('mongodb');
+const { config } = require('../config');
 const createApp = require('../app');
-const { generateManyBook } = require('../fakes/book.fake');
-
-const mockFakeBooks = generateManyBook(5);
-jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-  getAll: () => mockFakeBooks,
-  create: () => {},
-})));
 
 describe('Test for books', () => {
   let app = null;
   let server = null;
+  let db = null;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     app = createApp();
-    server = app.listen(3001);
+    server = app.listen(3003);
+    const client = new MongoClient(config.dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    db = client.db(config.dbTestName);
   });
 
   afterAll(async () => {
     await server.close();
+    await db.collection('books').drop();
   });
 
   describe('Test endpoint [GET] /api/v1/books', () => {
-    test('shounld return a list books', async () => request(app)
-      .get('/api/v1/books')
-      .expect(200)
-      .then(({ body }) => {
-        expect(body.length).toEqual(5);
-      }));
+    test('shounld return a list books', async () => {
+      const seedData = await db.collection('books').insertMany([
+        {
+          _id: 0,
+          year: 1999,
+          author: 'Facu',
+        },
+        {
+          _id: 1,
+          year: 2010,
+          author: 'Juan',
+        },
+        {
+          _id: 2,
+          year: 2020,
+          author: 'Maria',
+        },
+      ]);
+
+      console.log(seedData);
+
+      return request(app)
+        .get('/api/v1/books')
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.length).toEqual(seedData.insertedCount);
+        });
+    });
   });
 });
